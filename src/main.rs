234fd9d0d21631feb931617281f7d602f2e69fe3
Rotem Carr <155629245+RotemCarr@users.noreply.github.com>
2026-03-06 19:34:07 +0200

    Remove tests from main binary

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@
 
 #![feature(custom_test_frameworks)]
 #![test_runner(pico_os::test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use cortex_m_rt::entry;
 
@@ -29,9 +28,6 @@ fn main() -> ! {
         init();
         uart::uart_init(115200);
 
-        #[cfg(test)]
-        test_main();
-
         let led = Pin::<25>::new();
         println!("Hello, World!");
         println!("Type a character: ");
@@ -49,42 +45,5 @@ fn main() -> ! {
             led.clear();
             wait_ms(500);
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use pico_os::{bit, print, println};
-    use pico_os::uart::{putc, puts};
-
-    #[test_case]
-    fn test_bit_macro() {
-        assert_eq!(bit(3), 0b1000);
-        assert_eq!(bit(0), 1);
-        assert_eq!(bit(3), 0b1000);
-        assert_eq!(bit(31), 0x8000_0000);
-        assert_ne!(bit(7), 0x0001);
-        assert_ne!(bit(2), 10000)
-    }
-
-    #[test_case]
-    fn test_uart_put_char() {
-        putc(b'A');
-
-        // Teardown
-        puts("\r\n");
-    }
-
-    #[test_case]
-    fn test_print() {
-        print!("Test print...");
-
-        // Teardown
-        puts("\r\n");
-    }
-
-    #[test_case]
-    fn test_println() {
-        println!("Test println...");
     }
 }
